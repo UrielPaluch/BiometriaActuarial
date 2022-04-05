166c6a7aeff8b40713c18ca04bc79ebdf63f5c70
UrielPaluch <uripaluch2@gmail.com>
2022-04-05 19:51:33 -0300

    Corrección para que coincida con el ejercicio 12

--- Mackeham/server.R
+++ Mackeham/server.R
@@ -13,13 +13,13 @@ shinyServer(function(input, output) {
 
     output$table_makeham <- renderDataTable( {
        
-        t <- seq(from = input$age_range[1], to = input$age_range[2], by = 1)
+        x <- seq(from = input$age_range[1], to = input$age_range[2], by = 1)
         
         s <- expression(exp(-input$param_A * t - input$param_B / log(input$param_C) * input$param_C^x * (input$param_C^t - 1)))
         
-        s_x_t <- eval(s, list(x = input$x_value, t = t))
+        s_x_t <- eval(s, list(x = x, t = 1))
         
-        data.frame(t = t, `tPx` = s_x_t)
+        data.frame(t = x, `tPx` = s_x_t)
         
 
     }, options = list(pageLength = 5))
